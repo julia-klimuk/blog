0a3f95b870a3bca24f7353ab97d01b6acaacb75e
Julia Klimuk <julia.klimuk27@gmail.com>
2019-01-22 16:23:07 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+return if User.count > 0
+
+10.times do |i|
+  u = User.create(name: "User #{i}")
+  Post.create(user: u, title: "Post #{i} by #{u.name}", body: "This is my awesome post. By #{u.name}")
+end
+
+10.times do
+  Comment.create(post: Post.offset(rand(9)).first, user: User.offset(rand(9)).first, body: "This is great!")
+end
